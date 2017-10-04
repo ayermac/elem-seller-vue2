22c7f1c30368c2a15098d011b85157e1eab940b8
Jason <codemac@163.com>
2017-10-04 21:20:07 +0800

    完成商品详情页中的评价列表组件

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -26,7 +26,32 @@
         </transition>
       </div>
       <split v-show="food.info"></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品信息</h1>
+        <p class="text">{{ food.info }}</p>
+      </div>
       <split></split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <ratingselect @select="selectRating" @toggle="toggleContent" :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType, rating.text)" v-for="(rating, index) in food.ratings" class="rating-item border-1px" :key="index">
+              <div class="user">
+                <span class="name">{{ rating.username }}</span>
+                <img class="avatar" width="12" height="12" :src="rating.avatar">
+              </div>
+              <div class="time">{{ rating.rateTime | formatDate}}</div>
+              <p class="text">
+                <span :class="{'icon-thumb_up': rating.rateType===0, 'icon-thumb_down': rating.rateType===1}"></span>{{ rating.text }}
+              </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || !food.ratings.length">
+            暂无评价
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </transition>
@@ -35,8 +60,12 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import Vue from 'vue';
+  import {formatDate} from '../../common/js/date';
   import cartcontrol from '@/components/cartcontrol/cartcontrol';
   import split from '@/components/split/split';
+  import ratingselect from '@/components/ratingselect/ratingselect';
+
+  const ALL = 2;
 
   export default {
     props: {
@@ -46,12 +75,21 @@
     },
     data () {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show () {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = true;
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs.food, {
@@ -74,11 +112,40 @@
       },
       addFood (target) {
         this.$emit('add', target);
+      },
+      selectRating (type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      toggleContent () {
+        this.onlyContent = !this.onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      needShow (type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      }
+    },
+    filters: {
+      formatDate (time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
       }
     },
     components: {
       cartcontrol,
-      split
+      split,
+      ratingselect
     }
   };
 </script>
